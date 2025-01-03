24a5dad93a0d84d81297074a649c51e17845a21f
mohammed32145 <0mohamd0000@gmail.com>
2025-01-04 00:27:23 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,8 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename="/reactRouter">
+     <StrictMode>
     <App />
-  </BrowserRouter>,
+  </StrictMode>,
 
 )
